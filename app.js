9a380609963144b8ee5d845249a04390d2cc803d
Royer Maxence <A602147@fr01.awl.atosorigin.net>
2017-11-06 17:19:06 +0100

    [MRR] #End validation 1

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log("It works !");
+var express = require("express");
+var http = require("http");
+var CONFIG = require("./config.json");
+process.env.CONFIG = JSON.stringify(CONFIG); // module's access to the configuration
+var defaultRoute = require("./app/routes/default.route.js");
+var path = require("path");
+
+var app = express();
+app.use(defaultRoute);
+app.use("/admin", express.static(path.join(__dirname, "public/admin")));
+app.use("/watch", express.static(path.join(__dirname, "public/watch")));
+
+var server = http.createServer(app); // init the server
+server.listen(CONFIG.port);
+
+//console.log("It works !");
