ca1005f61cbf8620ff835e3c2b56469b0cce47b6
camillecordier <cordier.camille.cc@gmail.com>
2017-11-08 15:10:22 +0100

    [CCR]Try ContentModel but not conclude

--- app/models/content.model.js
+++ app/models/content.model.js
@@ -1,48 +1,71 @@
-public class ContentModel {
-  public type;
-  public id;
-  public title;
-  public src;
-  public fileName;
-  public data;
+const fs = require('fs');
+var CONFIG = require("../../config.json");
+const contentDirectory = CONFIG.contentDirectory;
 
-  /**
-  * Create
-  */
-  public static create(content, callback) {
-
-  }
-
-  /**
-  * Read
-  */
-  public static read(id, callback) {
-
-  }
-
-  /**
-  * Update
-  */
-  public static update(content, callback) {
-
-  }
-
-  /**
-  * Delete
-  */
-  public static delete(id, callback) {
-
-  }
-
-  /**
-  * Constructor
-  */
-  public ContentModel(jsonObject) {
+/**
+* Creation Class
+*/
+function ContentModel (jsonObject) {
     this.id = jsonObject.id;
     this.type = jsonObject.type;
     this.title = jsonObject.title;
     this.src = jsonObject.src;
     this.fileName = jsonObject.fileName;
-    this.data = jsonObject.data;
-  }
+    var data = jsonObject.data;
 }
+
+/**
+* Constructeur
+*/
+module.exports = function(param) {
+    return({
+        myInstance: new ContentModel(fs.readFileSync(filePath = contentDirectory + "/" + "62cf58dd-ecb1-495a-899c-b7c633fa1df7.meta.json"))
+    });
+}
+
+/**
+* Create
+*/
+ContentModel.prototype.create = function(content, callback) {
+    this.fileName = content.data
+    return this.str;
+};
+
+/**
+* Read
+*/
+ContentModel.prototype.read = function(id, callback) {
+    
+    return this.str;
+};
+
+/**
+* Update
+*/
+ContentModel.prototype.update = function(content, callback) {
+    
+    return this.str;
+};
+
+/**
+* Delete
+*/
+ContentModel.prototype.delete = function(id, callback) {
+    
+    return this.str;
+};
+
+/**
+* GetData
+*/
+ContentModel.getData = function() {
+    
+    return data;
+};
+
+/**
+* SetData
+*/
+ContentModel.setData = function(data) {
+    this._data = data;
+};
