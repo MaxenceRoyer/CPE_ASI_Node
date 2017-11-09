f6e0a99fb226aa291fe7065fc5e17aebddb975df
Royer Maxence <A602147@fr01.awl.atosorigin.net>
2017-11-09 16:40:15 +0100

    [MRR] Merge

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ var http = require("http");
 var CONFIG = require("./config.json");
 process.env.CONFIG = JSON.stringify(CONFIG); // module's access to the configuration
 var defaultRoute = require("./app/routes/default.route.js");
+<<<<<<< Updated upstream
 var presentationRoute = require("./app/routes/presentation.route.js");
+=======
+>>>>>>> Stashed changes
 var path = require("path");
-var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(defaultRoute);
+<<<<<<< Updated upstream
 app.use(presentationRoute);
+=======
+>>>>>>> Stashed changes
 app.use("/admin", express.static(path.join(__dirname, "public/admin")));
 app.use("/watch", express.static(path.join(__dirname, "public/watch")));
 
 var server = http.createServer(app); // init the server
 server.listen(CONFIG.port);
+
+//console.log("It works !");
